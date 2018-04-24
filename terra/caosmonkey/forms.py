77bd3a66a24beb03b9583ce9bcd994d70801ebb0
Alexandre DAmato <rocko83a@gmail.com>
2018-04-24 17:25:26 -0300

    caos form template generated find method replaced

--- terra/caosmonkey/forms.py
+++ terra/caosmonkey/forms.py
@@ -1,5 +1,3 @@
 from django import forms
 class CaosProject(forms.Form):
     projectname = forms.CharField(required=True,label='Project Name')
-    name = forms.CharField(required=True)
-    cloud_id = forms.IntegerField(required=True)
