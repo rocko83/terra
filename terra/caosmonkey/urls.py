830f09c18036b56c59788f41e5b0e1cbeb8948bb
Alexandre DAmato <rocko83a@gmail.com>
2018-04-25 17:53:45 -0300

    start of search project method for the caosmonkey app

--- terra/caosmonkey/urls.py
+++ terra/caosmonkey/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from caosmonkey import views as Views
 urlpatterns = [
-    url(r'caos/$', Views.caosmain, name='caosmain'),
-    url(r'caos/find/form',Views.caosmain_find_form,name = 'caosmain_find_form')
+    url(r'caos/$', Views.caosmain, name='caosmain')
 ]
--- terra/caosmonkey/views.py
+++ terra/caosmonkey/views.py
@@ -2,16 +2,36 @@ from django.shortcuts import render
 from caosmonkey.models import Caosmonkey
 from django.contrib import admin
 from .forms import CaosProject
+from .models import Caosmonkey
 admin.site.register(Caosmonkey)
 projectName="Caos Monkey"
+
 def caosmain(request):
-    form = CaosProject()
-    return render(request, 'caos_find.html', {'menu': 'caos', 'submenu': 'find','title':projectName,'form':form})
-def caosmain_find_form(request):
     if request.method == 'POST':
         form = CaosProject(request.POST)
         if form.is_valid():
-            print(form.cleaned_data['projectname'])
-    #return redirect('http://' + form.cleaned_data['projectname'])
-    return render(request, 'caos_find_result.html',
-                  {'menu': 'project', 'submenu': 'find', 'result':form.cleaned_data['projectname'],'title':projectName})
+            search = form.cleaned_data['projectname']
+            try:
+                results = Caosmonkey.objects.get(name__contains=search)
+                return render(request, 'caos_find.html',
+                              {'menu': 'project', 'submenu': 'find', 'results': results,
+                               'title': projectName, 'method': 'post'})
+            except:
+                print(search + " nao existe")
+            return render(request, 'caos_find.html',
+                          {'menu': 'project', 'submenu': 'find',
+                           'title': projectName, 'method': 'post'})
+    else:
+        form = CaosProject()
+        projects = []
+        for dados in Caosmonkey.objects.all().order_by('-creation'):
+            array2 = []
+            array2.append(dados.name)
+            array2.append(dados.filedata)
+            array2.append(dados.creation)
+            array2.append(dados.cloud_id)
+            array2.append(dados.id)
+            projects.append(array2)
+
+        return render(request, 'caos_find.html',
+                      {'menu': 'caos', 'submenu': 'find', 'method': 'web', 'title': projectName, 'form': form,'projects':projects})
